3797065a28632ce21d255343912238f86d3590f6
Iryna Konoplova <a.irina773@gmail.com>
2021-11-12 21:55:47 +0200

    adds a database

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express()
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger));
+app.use(cors());
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message })
+})
+
+module.exports = app
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const DB_HOST = "mongodb+srv://Iryna:pjfa9DtKd2YbLDqU@cluster0.yncdl.mongodb.net/wallet?retryWrites=true&w=majority"
+
+// mongoose.connect(DB_HOST)
+//   .then(() => {
+//     // app.listen(PORT)
+//     console.log('Database connection successful')
+//   })
+//   .catch(error => {
+//     console.log(error.message)
+//     process.exit(1);
+//   })
