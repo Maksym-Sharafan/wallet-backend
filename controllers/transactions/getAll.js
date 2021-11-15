f59f7541f412c65653470b2b2036d230aa61a1c3
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-11-15 21:46:55 +0200

    feature/endpoint-transactions-for-the-period

--- controllers/transactions/getAll.js
+++ controllers/transactions/getAll.js
@@ -2,14 +2,11 @@ const transactionsHandlers = require('../../services/transactions')
 
 const getAll = async (req, res, next) => {
   const { _id } = req.user
-  let { page = 1, limit = 5, month, year, category } = req.query
-  page = Number(page)
-  limit = Number(limit)
-  month = Number(month)
-  year = Number(year)
-  const skip = (page - 1) * limit
-  console.log( month, year)
-  const transactionList = await transactionsHandlers.transactionList(_id, skip, limit, month, year, category)
+  let { month, year, category } = req.query
+  // month = Number(month)
+  // year = Number(year)
+  // console.log(typeof month, typeof year)
+  const transactionList = await transactionsHandlers.transactionList(_id, month, year, category)
   res.json({ transactionList })
 };
 
--- services/transactions/transactionList.js
+++ services/transactions/transactionList.js
@@ -1,10 +1,20 @@
 const { Transaction } = require('../../models')
 
-const transactionList = async (userId, skip, limit, month, year, category) => {
+const transactionList = async (userId, month, year, category) => {
   try {
-// отбор по периоду добавить
-    const transactionList = await Transaction.find({ owner: userId}).skip(skip).limit(limit)
-    return transactionList
+    if (month&&year) {
+      // console.log(typeof month,  month)
+      //   console.log(typeof year, year)
+        const fromDate = `${year}-${month.length===1? `0${month}`: month}-01T00:00:00.000Z`;
+        const toDate = `${month === '12'?Number(year) + 1:year}-${month === '12'? '01': String(Number(month) + 1).length===1? `0${Number(month) + 1}`: Number(month) + 1}-01T00:00:00.000Z`
+        // console.log("fromDate", fromDate)
+        // console.log("toDate", toDate)
+      const transactionList = await Transaction.find({ owner: userId, date: { $gte: fromDate, $lt: toDate } })
+      return transactionList;
+    } else {
+      const transactionList = await Transaction.find({ owner: userId });
+      return transactionList;
+    }
   } catch (error) {
     throw error
   }
