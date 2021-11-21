b942ae688ac2ebb8fd736f359b0c1cc0ff57ea3b
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-11-21 21:50:12 +0200

    отменить обновления

--- services/transactions/addTransaction.js
+++ services/transactions/addTransaction.js
@@ -7,20 +7,9 @@ const moment = require('moment')
 const addTransaction = async (body, owner) => {
   try {
     const { type, category, amount, date, comment } = body;
-  //  moment(date).format();
-  //   console.log("date", date)
-  //   const transactionDate = moment(date).format();
-    // console.log("transactionDate", transactionDate)
-    // const transactionDate = new Date(date)
-    // console.log("transactionDate", transactionDate)
-    // const datex = new Date(transactionDate);
-    // console.log("datex", datex)
-    // const transactionDate = new Date(date.slice(0,10))
-    // const previousTransactions = await Transaction.find({ owner, date: { $lt: transactionDate } }).sort('date')
-    // const nextTransactions = await Transaction.find({ owner, date: { $gte: transactionDate } }).sort('date')
-    const transactionDate = new Date(date.slice(0,10))
-    const previousTransactions = await Transaction.find({ owner, date: { $lt: transactionDate } }).sort('date')
-    const nextTransactions = await Transaction.find({ owner, date: { $gte: transactionDate } }).sort('date')
+
+    const previousTransactions = await Transaction.find({ owner, date: { $lt: new Date(date) } }).sort('date')
+    const nextTransactions = await Transaction.find({ owner, date: { $gte: new Date(date) } }).sort('date')
     let balance = 0;
     
     if (previousTransactions.length===0) {
@@ -52,7 +41,7 @@ const addTransaction = async (body, owner) => {
         }
       };
     };
-    const newTransaction = new Transaction({ type, category, amount, date: transactionDate, comment,balance, owner })
+    const newTransaction= new Transaction({type, category, amount, date, comment,balance, owner })
     await newTransaction.save()
     
     const updatedTransactionsList = await transactionList(owner)
