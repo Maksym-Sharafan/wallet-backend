613a3bcb40d473410e069344af716ba801e56521
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-11-21 02:00:17 +0200

    fix type select

--- services/transactions/addTransaction.js
+++ services/transactions/addTransaction.js
@@ -2,10 +2,22 @@ const { BadRequest } = require('http-errors')
 
 const { Transaction, User } = require('../../models')
 const transactionList = require('./transactionList')
+const moment = require('moment')
 
 const addTransaction = async (body, owner) => {
   try {
     const { type, category, amount, date, comment } = body;
+  //  moment(date).format();
+  //   console.log("date", date)
+  //   const transactionDate = moment(date).format();
+    // console.log("transactionDate", transactionDate)
+    // const transactionDate = new Date(date)
+    // console.log("transactionDate", transactionDate)
+    // const datex = new Date(transactionDate);
+    // console.log("datex", datex)
+    // const transactionDate = new Date(date.slice(0,10))
+    // const previousTransactions = await Transaction.find({ owner, date: { $lt: transactionDate } }).sort('date')
+    // const nextTransactions = await Transaction.find({ owner, date: { $gte: transactionDate } }).sort('date')
     const transactionDate = new Date(date.slice(0,10))
     const previousTransactions = await Transaction.find({ owner, date: { $lt: transactionDate } }).sort('date')
     const nextTransactions = await Transaction.find({ owner, date: { $gte: transactionDate } }).sort('date')
