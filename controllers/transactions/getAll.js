6b4ecd18908088142dfeeee4bca9cc19a4a7a7d1
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-11-23 18:51:08 +0200

    cleaning

--- controllers/transactions/getAll.js
+++ controllers/transactions/getAll.js
@@ -3,9 +3,6 @@ const transactionsHandlers = require('../../services/transactions')
 const getAll = async (req, res, next) => {
   const { _id } = req.user
   let { month, year, category } = req.query
-  // month = Number(month)
-  // year = Number(year)
-  // console.log(typeof month, typeof year)
   const transactionList = await transactionsHandlers.transactionList(_id, month, year, category)
   res.json({ transactionList })
 };
--- services/users/getUserById.js
+++ services/users/getUserById.js
@@ -2,8 +2,6 @@ const { User } = require('../../models/user')
 
 const getUserById = async (id) => {
   const user = await User.findOne({ _id: id })
-  // console.log(user);
-
   if (user) {
     return user
   } else {
